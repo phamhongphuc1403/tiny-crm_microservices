623c3a0fe4999c529921fee7ed272b829a2837f5
el-intern <138639072+el-intern@users.noreply.github.com>
2023-09-12 17:53:12 +0700

    chore: config API Gateway (#6)
    Co-authored-by: Phúc Phạm <phamhongphuc1403@gmail.com>

--- TinyCRM/APIGateway/Program.cs
+++ TinyCRM/APIGateway/Program.cs
@@ -1,25 +1,16 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen();
+builder.Services.AddOcelot();
+builder.Services.AddSwaggerForOcelot(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
+app.UseSwagger();
+app.UseSwaggerForOcelotUI().UseOcelot();
 app.Run();
