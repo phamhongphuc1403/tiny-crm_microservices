089791b43ab008e61ad8c9907498ea7ec2d9ad0c
el-intern <138639072+el-intern@users.noreply.github.com>
2023-09-22 10:13:08 +0700

    chore: change phone number regex (#37)
    Co-authored-by: phamhongphuc1403 <phamhongphuc1403@gmail.com>

--- TinyCRM/Services/People/People.Application/Validations/CreateAccountCommandValidator.cs
+++ TinyCRM/Services/People/People.Application/Validations/CreateAccountCommandValidator.cs
@@ -29,6 +29,6 @@ public partial class CreateAccountCommandValidator : AbstractValidator<CreateAcc
             .MaximumLength(255);
     }
 
-    [GeneratedRegex(@"((\(\d{3}\) ?)|(\d{3}-))?\d{3}-\d{4}")]
+    [GeneratedRegex(@"(84|0[3|5|7|8|9])+([0-9]{8})\b")]
     private static partial Regex MyRegex();
 }
