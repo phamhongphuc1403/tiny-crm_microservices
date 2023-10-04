73700bd706de3ebb7099d442da4addf1c2b75a94
el-intern <138639072+el-intern@users.noreply.github.com>
2023-10-04 15:07:42 +0700

    chore(people): add account id in contact detail response (#87)
    Co-authored-by: phamhongphuc1403 <phamhongphuc1403@gmail.com>

--- TinyCRM/Services/People/People.Application/DTOs/ContactDTOs/ContactDetailDto.cs
+++ TinyCRM/Services/People/People.Application/DTOs/ContactDTOs/ContactDetailDto.cs
@@ -6,5 +6,6 @@ public class ContactDetailDto
     public string Name { get; set; } = null!;
     public string Email { get; set; } = null!;
     public string Phone { get; set; } = null!;
+    public Guid AccountId { get; set; }
     public string AccountName { get; set; } = null!;
 }
